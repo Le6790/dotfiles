ea6fc5dc1bb6ad73e49cc23f0209c35b08664cf9
Yanille <yanille@pop-os.localdomain>
2024-01-30 20:44:05 -0700

    small customizations

--- .wezterm.lua
+++ .wezterm.lua
@@ -23,16 +23,16 @@ config.keys = {
 }
 
 config.font = wezterm.font 'JetBrains Mono'
+config.warn_about_missing_glyphs=false
 config.font_size = 10.0
 config.color_scheme = "Kanagawabones"
-config.window_background_opacity = 1
-config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+--config.window_decorations = 'INTEGRATED_BUTTONS'
 config.canonicalize_pasted_newlines= nil
 config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.4}
 config.window_background_image_hsb = { brightness = 0.05, hue = 1.0, saturation = 1.0}
 config.window_background_opacity = 0.95
 config.force_reverse_video_cursor = true
-config.text_background_opacity = .8
+config.text_background_opacity = 0.8
 config.foreground_text_hsb = {
   hue = .90,
   saturation = 1.5,
