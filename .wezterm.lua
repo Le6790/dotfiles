00dc44c103c7f2c85a075411d2f433d2fa50ed2a
Kent Le <le6790@gmail.com>
2026-03-10 00:27:43 -0600

    fixed file format for .wezterm and updated tmux.conf background color

--- .wezterm.lua
+++ .wezterm.lua
@@ -21,22 +21,22 @@ config.keys = {
 	},
 }
 
-config.enable_scroll_bar = false
-config.mouse_bindings = {
-	-- Disable scrolling when NOT in alt-screen (shell, prompts)
-	{
-		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-		mods = "NONE",
-		alt_screen = false,
-		action = "DisableDefaultAssignment",
-	},
-	{
-		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-		mods = "NONE",
-		alt_screen = false,
-		action = "DisableDefaultAssignment",
-	},
-}
+--config.enable_scroll_bar = false
+-- config.mouse_bindings = {
+-- 	-- Disable scrolling when NOT in alt-screen (shell, prompts)
+-- 	{
+-- 		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+-- 		mods = "NONE",
+-- 		alt_screen = false,
+-- 		action = "DisableDefaultAssignment",
+-- 	},
+-- 	{
+-- 		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+-- 		mods = "NONE",
+-- 		alt_screen = false,
+-- 		action = "DisableDefaultAssignment",
+-- 	},
+-- }
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
@@ -47,7 +47,7 @@ config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.4 }
 config.window_background_image_hsb = { brightness = 0.05, hue = 1.0, saturation = 1.0 }
 config.window_background_opacity = 0.95
 config.force_reverse_video_cursor = true
-config.text_background_opacity = 0.8
+config.text_background_opacity = 1.0
 config.foreground_text_hsb = {
 	hue = 0.90,
 	saturation = 1.5,
